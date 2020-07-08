5ed260cee59a704a1651d7fa80ec8ae09cbd2f80
jreiss1923 <joshua.reiss2@gmail.com>
2020-07-08 00:27:24 -0400

    Added twitter api

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import twitter
 import speedtest
 
-st = speedtest.Speedtest()
+codes = open("twitter_codes.txt", "r")
+ACCESS_TOKEN = codes.readline().rstrip()
+ACCESS_SECRET = codes.readline().rstrip()
+KEY = codes.readline().rstrip()
+KEY_SECRET = codes.readline().rstrip()
 
-print(st.download()/1000000)
+api = twitter.Api(KEY, KEY_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+st = speedtest.Speedtest()
+#print(st.download()/1000000)
+
+status = api.PostUpdate("My current internet speed is " + str(st.download()/1000000) + "Mbps")
+
+
 
 
 
