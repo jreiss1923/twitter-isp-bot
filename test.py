b7479cd713defbf8d3ec01ed5d2903de0773424e
jreiss1923 <joshua.reiss2@gmail.com>
2020-07-08 13:42:39 -0400

    Automatically run using python time

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import twitter
 import speedtest
+import time
 
 codes = open("twitter_codes.txt", "r")
 ACCESS_TOKEN = codes.readline().rstrip()
@@ -10,9 +11,10 @@ KEY_SECRET = codes.readline().rstrip()
 api = twitter.Api(KEY, KEY_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 st = speedtest.Speedtest()
-#print(st.download()/1000000)
 
-status = api.PostUpdate("My current internet speed is " + str(st.download()/1000000) + "Mbps")
+while True:
+    print(api.PostUpdate("My current internet speed is " + str(round(st.download()/1000000, 2)) + " Mbps"))
+    time.sleep(3600)
 
 
 
