a8df936e194ec55644305f0e197b94531863d2cc
jreiss1923 <joshua.reiss2@gmail.com>
2020-07-09 16:47:23 -0400

    Tags company if internet slow

--- test.py
+++ test.py
@@ -7,11 +7,18 @@ ACCESS_SECRET = codes.readline().rstrip()
 KEY = codes.readline().rstrip()
 KEY_SECRET = codes.readline().rstrip()
 
+UPLOAD_SPEED = 5.0
+DOWNLOAD_SPEED = 25.0
+
 api = twitter.Api(KEY, KEY_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 st = speedtest.Speedtest()
+dl = round(st.download()/1000000, 2)
+ul = round(st.upload()/1000000, 2)
 
-print(api.PostUpdate("My current internet speed is " + str(round(st.download()/1000000, 2)) + " Mbps"))
+if ul < UPLOAD_SPEED * 0.75 or dl < DOWNLOAD_SPEED * 0.75:
+    print(api.PostUpdate("@Optimum @OptimumHelp I pay for 25 download 5 upload but I'm getting " +
+                         str(dl) + " down and " + str(ul) + " up mbps"))
 
 
 
